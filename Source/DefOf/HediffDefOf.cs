9c1d73238046c04561316fe0ab0ca2aaf08aabc1
tenguin <73805320+tenguin@users.noreply.github.com>
2022-10-30 01:09:22 -0400

    Update for Rimworld 1.4

---
+++ Source/DefOf/HediffDefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace UsefulTerror
+{
+    [DefOf]
+    public static class HediffDefOf
+    {
+        public static HediffDef UsefulTerror_HeartAcidifier;
+
+        static HediffDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(HediffDefOf));
+        }
+    }
+}
